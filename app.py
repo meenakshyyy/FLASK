549ed791f3691c6dcce310edd73919e580f2ba84
meenakshyyy <meenakshys05@gmail.com>
2024-12-20 11:21:09 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_name():
-    return render_template("hello.html")
+def my_form():
+    return render_template("Hello.html")
+
+@app.route("/",methods=["POST"])
+def my_form_post():
+    text=request.form["text"]
+    return"<h1> {} </h1>".format(text)
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
